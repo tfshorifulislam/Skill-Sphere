76a22165dd5bd67864973091dc799cd3f46f6b5a
tfshorifulislam <tfshorifulislam@gmail.com>
2026-05-01 00:59:17 +0600

    final deploy

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     
-    baseURL: "http://localhost:3000"
+    baseURL: "https://skill-sphere-topaz.vercel.app"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
