e90b8d168a164351277e50da60315135b2d8c0c8
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-30 15:23:18 +0600

    card details page done

--- src/app/courses/[id]/page.jsx
+++ src/app/courses/[id]/page.jsx
@@ -1,7 +1,8 @@
 
 import React from 'react';
-import { Card,  } from '@heroui/react';
+import { Card, Chip, Separator, } from '@heroui/react';
 import Link from 'next/link';
+import { IoStar, IoTime } from 'react-icons/io5';
 const CoursesDetailsPage = async ({ params }) => {
     const { id } = await params;
     console.log(id)
@@ -11,13 +12,32 @@ const CoursesDetailsPage = async ({ params }) => {
     console.log(course)
     return (
         <Card className="w-11/12 mx-auto mt-10 p-5 flex flex-col  md:flex-row">
-            <img src={course.image} className='md:w-100 rounded-xl' alt="" />
+            <img
+                src={course.image}
+                className='md:w-100 rounded-xl' alt="" />
             <Card.Header>
                 <Card.Title
-                className='font-medium md:font-semibold text-lg md:text-xl text-[#313131] mb-4'>{course.title}</Card.Title>
+                    className='font-medium md:font-semibold text-lg md:text-xl text-[#313131] mb-4'>{course.title}</Card.Title>
                 <Card.Description>
                     {course.description}
                 </Card.Description>
+                <p className='mt-3'>
+                    instructor: {course.instructor}
+                </p>
+                <div className='flex gap-5  md:flex-row mt-5'>
+                    <Chip>{course.category}</Chip>
+                    <Chip>{course.level}</Chip>
+                </div>
+                <div className='flex gap-8  md:flex-row mt-6'>
+                    <span className='flex items-center gap-2 md:text-lg'>
+                        <IoTime /> {course.duration}
+                    </span>
+                    <Separator orientation="vertical" />
+                    <span className='flex items-center gap-2 md:text-lg'>
+                        <IoStar /> {course.rating}
+
+                    </span>
+                </div>
             </Card.Header>
         </Card>
     );
