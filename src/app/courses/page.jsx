cfb085a088da93bd90565230f096fa0735a02a82
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-30 10:31:18 +0600

    all courses dynamically show done

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -1,11 +1,14 @@
+import CoursesCard from '@/components/CoursesCard';
 import React from 'react';
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+    const res = await fetch('https://skill-sphere-topaz.vercel.app/data.json');
+    const courses = await res.json();
     return (
-        <div>
-            <h1>
-                this is Courses page
-            </h1>
+        <div className='grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-5 w-11/12 mx-auto mt-10'>
+            {
+                courses.map(course => <CoursesCard key={course.id} course={course} />)
+            }
         </div>
     );
 };
