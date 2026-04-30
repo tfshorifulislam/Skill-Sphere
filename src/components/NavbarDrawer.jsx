f600ad031ba15aa6ed528b692840583755a12597
tfshorifulislam <tfshorifulislam@gmail.com>
2026-05-01 01:12:51 +0600

    drawer auto close system setup

--- src/components/NavbarDrawer.jsx
+++ src/components/NavbarDrawer.jsx
@@ -24,36 +24,42 @@ export function NavbarDrawer() {
             <Drawer.Backdrop isOpen={isOpen} onOpenChange={setIsOpen}>
                 <Drawer.Content placement="right">
                     <Drawer.Dialog>
-                       <Drawer.Header>
-                        <Drawer.Heading>Profile</Drawer.Heading>
-                        <div>
-                            {!user && <div className="w-full space-y-5 flex flex-col">
-                                <Link href={'/login'}>
-                                    <Button variant="primary" className='w-full' >Login</Button>
-                                </Link>
-                                <Link href={'/register'}>
-                                    <Button variant="outline" className='w-full' >Register</Button>
-                                </Link>
-                            </div>}
-                            {user && <div className="px-5">
-                                <Link href={'/login'}>
-                                    <Button variant="danger" className=' absolute bottom-5 right-5 '>Logout</Button>
-                                </Link>
+                        <Drawer.Header>
+                            <Drawer.Heading>Profile</Drawer.Heading>
+                            <div>
+                                {!user && <div className="w-full space-y-5 flex flex-col">
+                                    <Link href={'/login'}>
+                                        <Button variant="primary" className='w-full' >Login</Button>
+                                    </Link>
+                                    <Link href={'/register'}>
+                                        <Button variant="outline" className='w-full' >Register</Button>
+                                    </Link>
+                                </div>}
+                                {user && <div className="px-5">
+                                    <Link href={'/login'}>
+                                        <Button variant="danger" className=' absolute bottom-5 right-5 '>Logout</Button>
+                                    </Link>
 
-                            </div>}
-                            {user && <ul className="flex flex-col space-y-3">
-                                <Button className=" w-full rounded-lg" variant="outline">
-                                    <Link href={'/'}>Home</Link>
-                                </Button>
-                                <Button className=" w-full rounded-lg" variant="outline">
-                                    <Link href={'/courses'}>Courses</Link>
-                                </Button>
-                                <Button className=" w-full rounded-lg" variant="outline">
-                                    <Link href={'/profile'}>My Profile</Link>
-                                </Button>
-                            </ul>}
-                        </div>
-                       </Drawer.Header>
+                                </div>}
+                                {user && <ul className="flex flex-col space-y-3">
+                                    <Button
+                                        onClick={() => setIsOpen(false)}
+                                        className=" w-full rounded-lg" variant="outline">
+                                        <Link href={'/'}>Home</Link>
+                                    </Button>
+                                    <Button
+                                        onClick={() => setIsOpen(false)}
+                                        className=" w-full rounded-lg" variant="outline">
+                                        <Link href={'/courses'}>Courses</Link>
+                                    </Button>
+                                    <Button
+                                        onClick={() => setIsOpen(false)}
+                                        className=" w-full rounded-lg" variant="outline">
+                                        <Link href={'/profile'}>My Profile</Link>
+                                    </Button>
+                                </ul>}
+                            </div>
+                        </Drawer.Header>
                     </Drawer.Dialog>
                 </Drawer.Content>
             </Drawer.Backdrop>
